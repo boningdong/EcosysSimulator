bb6206eaed0f1fcc3a34a3c2a17913c507008d54
Boning <dbndbn9538@gmail.com>
2018-07-02 18:37:21 -0700

    Added finding mate target function.

--- Entities.py
+++ Entities.py
@@ -10,29 +10,33 @@ class Animal:
         self.worldHeight = height
 
         # Animal Attributes
-        self.size = 15
+        self.size = 10
         # - Parameters
         self.hungerRate = 1
         self.maxFood = 100
-        self.hungerThreshold = 85
-        self.maxAge = 100
-        self.matureAge = 15
+        self.hungerThreshold = 80
+        self.boostFactor = random.randrange(13, 17) / 10.0
+        self.hungerFactor = self.boostFactor ** (1/1.5)
+        self.pregProbability = 0.7
+        self.maxAge = 30
+        self.matureAge = 5
         self.alertRange = 40
         self.desireThreshold = 60
         self.maxDesire = 100
-        self.desireRate = 40
+        self.desireRate = 20
 
 
         # Stauts
         # - Location
         self.x = x if x != None else random.randrange(0, width)
         self.y = y if y != None else random.randrange(0, height)
-        self.v = random.randrange(30, 40)
+        self.v = random.randrange(20, 40)
         self.angle = random.randrange(0, 180) / math.pi
         # - Target
         self.tx = random.randrange(0, self.worldWidth)
         self.ty = random.randrange(0, self.worldHeight)
-        # - Health
+        self.mateTarget = None
+        # - Status
         self.dead = False
         self.isMature = False
         self.food = 100
@@ -74,48 +78,70 @@ class Animal:
         y_dis = self.ty - self.y
         self.angle = math.atan2(y_dis, x_dis)
 
+    def RunToward(self, target, dt, reverse = False):
+        x_dis = target.x - self.x
+        y_dis = target.y - self.y
+        self.angle = math.atan2(y_dis, x_dis)
+        self.angle = self.angle + math.pi if reverse else self.angle
+        self.x += self.v * self.boostFactor * math.cos(self.angle) * dt
+        self.y += self.v * self.boostFactor * math.sin(self.angle) * dt
+
+    def Mate(self, target):
+        target.desire = 0
+        target.mateTarget = None
+        self.desire = 0
+        self.mateTarget = None
+
+
     def isHunger(self):
         return self.food < self.hungerThreshold
 
     def isDesireStrong(self):
         return self.desire > self.desireThreshold
 
+
 class Sheep (Animal):
     def __init__(self, width, height, x = None, y = None):
         Animal.__init__(self, width, height, x, y)
 
-        # Sheep Attributes:
-        self.boostFactor = 1.5
-        self.hungerFactor = self.boostFactor
-        self.pregProbability = 0.7
+        # Sheep Attributes
+        self.foodValue = random.randrange(3, 8)
 
         # Sheep Status
         self.danger = None
 
     def Update(self, dt):
         Animal.Update(self, dt)
+        # Non danger status
         if self.danger == None:
-            if (self.GetTargetDistance() < self.v):
-                self.NewTarget()
-            self.MoveToTarget()
-            self.x += self.v * math.cos(self.angle) * dt
-            self.y += self.v * math.sin(self.angle) * dt
-            self.food -= self.hungerRate * dt
+            if self.mateTarget == None:
+                # No mate target
+                if (self.GetTargetDistance() < self.v):
+                    self.NewTarget()
+                self.MoveToTarget()
+                self.x += self.v * math.cos(self.angle) * dt
+                self.y += self.v * math.sin(self.angle) * dt
+                self.food -= self.hungerRate * dt
+            else:
+                # has mate target
+                self.RunToward(self.mateTarget, dt)
+                self.food -= self.hungerRate * dt * self.hungerFactor
+                if self.CheckDistanceTo(self.mateTarget) > self.alertRange:
+                    self.mateTarget = None
+
+        # In danger status
         else:
-            x_dis = self.danger.x - self.x
-            y_dis = self.danger.y - self.y
-            self.angle = math.atan2(y_dis, x_dis) + math.pi
-            self.x += self.v * self.boostFactor * math.cos(self.angle) * dt
-            self.y += self.v * self.boostFactor * math.sin(self.angle) * dt
-            self.age += 1 * dt
+            self.RunToward(self.danger, dt, reverse = True)
             self.food -= self.hungerRate * dt * self.hungerFactor
-            if self.CheckDangerDistance() > self.alertRange:
+            if self.CheckDistanceTo(self.danger) > self.alertRange:
+                self.NewTarget()
                 self.danger = None
+        #
+        # Check death
         if self.food <= 0 or self.age > self.maxAge: self.dead = True
 
     def Mate(self, s):
-        s.desire = 0
-        self.desire = 0
+        Animal.Mate(self, s)
         if random.random() < self.pregProbability:
             return Sheep(self.worldWidth, self.worldHeight, self.x, self.y)
         else:
@@ -125,48 +151,34 @@ class Sheep (Animal):
         # DEBUG Can replace wolf with wolfs
         self.danger = wolf
 
-    def CheckDangerDistance(self):
-        if self.danger == None:
-            return None
-        else:
-            return math.sqrt((self.danger.x - self.x)**2 + (self.danger.y - self.y)**2)
-
-
 class Wolf (Animal):
     def __init__(self, width, height, x = None, y = None):
         Animal.__init__(self, width, height, x, y)
-
-        # Wolf Attributes:
-        self.boostFactor = 2
-        self.hungerFactor = self.boostFactor
-        self.sheepFoodValue = 5
-        self.pregProbability = 0.6
-
         # Wolf Status
         self.target = None
 
     def Update(self, dt):
         Animal.Update(self, dt)
         if self.target == None:
-            if (self.GetTargetDistance() < self.v):
-                self.NewTarget()
-            self.MoveToTarget()
-            self.x += self.v * math.cos(self.angle) * dt
-            self.y += self.v * math.sin(self.angle) * dt
-            self.food -= self.hungerRate * dt
+            if self.mateTarget == None:
+                if (self.GetTargetDistance() < self.v):
+                    self.NewTarget()
+                self.MoveToTarget()
+                self.x += self.v * math.cos(self.angle) * dt
+                self.y += self.v * math.sin(self.angle) * dt
+                self.food -= self.hungerRate * dt
+            else:
+                self.RunToward(self.mateTarget, dt)
+                self.food -= self.hungerRate * dt * self.hungerFactor
+                if self.CheckDistanceTo(self.mateTarget) > self.alertRange:
+                    self.mateTarget = None
         else:
-            x_dis = self.target.x - self.x
-            y_dis = self.target.y - self.y
-            self.angle = math.atan2(y_dis, x_dis)
-            self.x += self.v * self.boostFactor * math.cos(self.angle) * dt
-            self.y += self.v * self.boostFactor * math.sin(self.angle) * dt
-            self.age += 1 * dt
+            self.RunToward(self.target, dt)
             self.food -= self.hungerRate * dt * self.hungerFactor
         if self.food <= 0 or self.age > self.maxAge: self.dead = True
 
     def Mate(self, w):
-        w.desire = 0
-        self.desire = 0
+        Animal.Mate(self, w)
         if random.random() < self.pregProbability:
             print("Wolf mate success")
             return Wolf(self.worldWidth, self.worldHeight, self.x, self.y)
@@ -177,8 +189,7 @@ class Wolf (Animal):
         # DEBUG sheep can be replaced with sheeps, ask the wolf to choose which one to pick
         if self.isHunger():
             self.target = sheep
-        else:
-            print (self.food)
+
 
     def CheckSheepDistance(self):
         if self.target == None:
@@ -187,6 +198,6 @@ class Wolf (Animal):
             return math.sqrt((self.target.x - self.x)**2 + (self.target.y - self.y)**2)
 
     def EatSheep(self):
-        self.food += 10
+        self.food += self.target.foodValue
         self.target.dead = True
         self.target = None
--- World.py
+++ World.py
@@ -2,7 +2,7 @@ from Entities import *
 from Utilities import Timer, Point
 
 INIT_SHEEPS_NUM = 40
-INIT_WOLF_NUM = 7
+INIT_WOLF_NUM = 15
 
 class World:
     def __init__(self, width, height):
@@ -30,29 +30,43 @@ class World:
     def Update(self):
         self.timer.Tick() # Update game timer
 
+        for s in self.sheeps:
+            s.Update(1 / self.fps)
+            self.Reposition(s)
+
         # Sheep alert
-        for sheep in self.sheeps:
-            sheep.Update(1 / self.fps)
+        for s in self.sheeps:
             for wolf in self.wolfs:
-                if sheep.danger == None and \
-                self.DistanceBetween(sheep, wolf) < sheep.alertRange:
-                    sheep.Escape(wolf)
+                if s.danger == None and \
+                self.DistanceBetween(s, wolf) < s.alertRange:
+                    s.Escape(wolf)
         # Sheep mate
         newSheeps = []
         for i in range(len(self.sheeps)):
             for k in range(i+1, len(self.sheeps)):
                 s1 = self.sheeps[i]
+                if not s1.isDesireStrong():
+                    s1.mateTarget = None
+                    break
+                # if it already has a target in range, then stick with the target.
+                if s1.mateTarget != None:
+                    if s1.CheckDistanceTo(s1.mateTarget) < self.mateRange:
+                        ss = s1.Mate(s1.mateTarget)
+                        if ss != None:
+                            newSheeps.append(ss)
+                    break
+                # if it has no target, then locate a new target.
                 s2 = self.sheeps[k]
-                if s1.CheckDistanceTo(s2) < self.mateRange and s1.isDesireStrong() and s2.isMature:
-                    sBaby = s1.Mate(s2)
-                    if sBaby != None:
-                        newSheeps.append(sBaby)
-                        break
+                if s1.CheckDistanceTo(s2) < s1.alertRange and s2.isMature:
+                    print ("[Sheep] => Located mate target!") # DEBUG
+                    s1.mateTarget = s2
 
+        for wolf in self.wolfs:
+            wolf.Update(1 / self.fps) # Move the wolf
 
         # wolves alert
         for wolf in self.wolfs:
-            wolf.Update(1 / self.fps) # Move the wolf
+
             for sheep in self.sheeps:
                 # Locate a target for the wolf
                 if wolf.target == None and \
@@ -68,12 +82,19 @@ class World:
         for i in range(len(self.wolfs)):
             for k in range(i+1, len(self.wolfs)):
                 w1 = self.wolfs[i]
+                if not w1.isDesireStrong():
+                    break
+                #  if it already has a target in range, then stick with the target.
+                if w1.mateTarget != None:
+                    if w1.CheckDistanceTo(w1.mateTarget) < self.mateRange and w1.isDesireStrong():
+                        ww  = w1.Mate(w1.mateTarget)
+                        if ww != None:
+                            newWolves.append(ww)
+                # if it has no mate target, then locate a new target.
                 w2 = self.wolfs[k]
-                if w1.CheckDistanceTo(w2) < self.mateRange and w1.isDesireStrong():
-                    wBaby = w1.Mate(w1)
-                    if wBaby != None:
-                        newWolves.append(wBaby)
-                        break
+                if w1.CheckDistanceTo(w2) < w1.alertRange and w2.isMature:
+                    print ("[Wolf] => Located mate target!") # DEBUG
+                    w1.mateTarget = w2
 
         survivedSheeps = []
         survivedWolfs = []
@@ -90,3 +111,9 @@ class World:
 
     def DistanceBetween(self, a1, a2):
         return math.sqrt((a2.x - a1.x)**2 + (a2.y - a1.y)**2)
+
+    def Reposition(self, obj):
+        if obj.x > self.width or obj.x < 0 or obj.y > self.height or obj.y < 0:
+            obj.x = obj.x % self.width if obj.x > self.width or obj.x < -1 else obj.x
+            obj.y = obj.y % self.height if obj.y > self.height or obj.y < -1 else obj.y
+            obj.NewTarget()
